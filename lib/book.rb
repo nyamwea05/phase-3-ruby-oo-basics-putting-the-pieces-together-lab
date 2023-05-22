cd92f4e1d4fee6f8ad36eac93feac547a894f1d5
sandra <nyamweasandra@gmail.com>
2023-05-22 15:05:31 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
-
+class Book
+    attr_reader :title, :author, :page_count, :genre
+  
+    def initialize(title, author, page_count, genre)
+      @title = title
+      @author = author
+      @page_count = page_count
+      @genre = genre
+    end
+  
+    def turn_page
+      # Implementation for turning the page
+    end
+  end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand, :color, :size, :material
+    attr_accessor :condition
+  
+    def initialize(brand, color, size, material, condition = "new")
+      @brand = brand
+      @color = color
+      @size = size
+      @material = material
+      @condition = condition
+    end
+  
+    def cobble
+      puts "The shoe has been repaired."
+      self.condition = "new"
+    end
+  end
